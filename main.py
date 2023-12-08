828a274368bd88a8f6e69bcd97c4f22205bb8ba7
anonymous5629 <153315430+anonymous5629@users.noreply.github.com>
2023-12-08 19:41:24 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 #!/usr/bin/python3
 
-passwdlen = input("Password Length:")
+import random 
 
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "1234567890"
+special_characters = "!£$%^&*()-_=+[{]};:'@#~,<.>/?\\|"
+#characters used in password 
+
+length = int(input("Password Length: "))
+amount = int(input("Amount of Passwords: "))
+print_to_file = bool(input("Print to File(answer as booleen)? "))
+if print_to_file == True:
+	filename = input("File Name: ")
+
+upper,lower,nums,spec_chars = True,True,True,True
+#edit booleans in order depending on what you characters want in the password(s)
+
+all = ""
+#where the characters are sampled from
+
+if upper == True:
+	all += uppercase_letters
+if lower == True:
+	all += lowercase_letters
+if nums == True:
+	all += digits
+if spec_chars == True:
+	all += special_characters
+
+for x in range(int(amount)):
+	password = "".join(random.sample(all,length))
+	print (password)
+#randomly samples characters from variable all the amount of times variable length specifies
+#and does this the number of times variable specifies
+	if print_to_file == True:
+		with open(filename, "a") as f:
+			f.write(password)
+			f.write("\n")
