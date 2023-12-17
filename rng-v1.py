f69444c094e0766f38ac6da6ae13a0dd49275af3
T <153315430+anonymous5629@users.noreply.github.com>
2023-12-17 22:16:35 +0000

    Update rng-v1.py

--- rng-v1.py
+++ rng-v1.py
@@ -16,9 +16,9 @@ for x in range(amount):
 	number = round(uniform(lowerb, upperb), decimal)
 	numlist.append(number)
 numlst = ("\n".join(map(str, numlist)))
-print(numlst)
 if print_to_file == True:
 	with open(filename, "a") as f:
 		f.write(numlst)			
+print(numlst)
 input()
 # Keeps terminal open after; can be removed if running from command line i.e. python3 rng-v1.py
