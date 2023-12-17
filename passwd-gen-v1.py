947637df4afadefdd24cd656a8b8239220aad8bc
T <153315430+anonymous5629@users.noreply.github.com>
2023-12-17 21:39:27 +0000

    Update passwd-gen-v1.py

--- passwd-gen-v1.py
+++ passwd-gen-v1.py
@@ -5,7 +5,6 @@ lowercase = uppercase.lower()
 digits = "1234567890"
 spec_chars = "!£$%^&*()-_=+[{]};:\'@#~,<.>/?\\|"
 #characters used in password 
-
 length = int(input("Password Length: "))
 amount = int(input("Amount of Passwords: "))
 print_to_file = input("Print to File? ")
@@ -14,15 +13,20 @@ if print_to_file in ["Yes", "yes"]:
 	print_to_file = True
 
 all = (uppercase + lowercase + digits + spec_chars)
+if length > 99:
+	all = all*length
 #where the characters are sampled from; edit depending on what you characters want in the password(s)
+passlist = []
 
 for x in range(amount):
 	password = "".join(sample(all, length))
-	print (password)
+	passlist.append(password)
+	
+passlst = "\n".join(passlist)
+print(passlst)
 #randomly samples characters from variable 'all'
-	if print_to_file == True:
-		with open(filename, "a") as f:
-			f.write(password)
-			f.write("\n")
+if print_to_file == True:
+	with open(filename, "a") as f:
+		f.write(passlst)
 input() 
 # Keeps terminal open after; can be removed if running from command line i.e. python3 passwd-gen-v1.py
