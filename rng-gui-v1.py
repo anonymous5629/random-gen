243381623172f01324a062a404826a13ed1c0ed9
T <153315430+anonymous5629@users.noreply.github.com>
2023-12-18 22:16:06 +0000

    now states numbers in gui

--- rng-gui-v1.py
+++ rng-gui-v1.py
@@ -12,40 +12,43 @@ def rng():
 		filename = filenm.get()
 		with open(filename, "a") as f:
 			f.write(numlst)
-	print(numlst)
-	
+	print_gui(numlst)
+
+def print_gui(numlst):
+	num_label = tk.Label(gui, text=("Numbers:\n"+numlst), font=("arial semibold", 14)).grid(row=9, rowspan=3, column=0, columnspan=2)
+
 gui = tk.Tk()
-gui.geometry("550x500")
+gui.geometry("700x650")
 gui.title("rng-gui-v1")
 
 gui.columnconfigure(0, weight=1)
 gui.columnconfigure(1, weight=1)
 
-for x in range(9):
+for x in range(12):
 	gui.rowconfigure(x, weight=1)
 
-label = tk.Label(gui, text="Random Number Generator V1\nGUI Version", font=("Arial Black", 19)).grid(row=0, rowspan=2, column=0, columnspan=2, sticky="nwes")
+label = tk.Label(gui, text="Random Number Generator V1\nGUI Version", font=("Arial Black", 19)).grid(row=0, column=0, columnspan=2, sticky="nwes")
 
 upb, lowb, deci, amot, ptf, filenm = tk.StringVar(), tk.StringVar(), tk.IntVar(), tk.IntVar(), tk.BooleanVar(), tk.StringVar()
 
-ub_lable = tk.Label(gui, text="Upper Bound: ", font=("arial semibold", 14)).grid(row=2, column=0, sticky="e")
-ub = tk.Entry(gui, textvariable = upb, font=("arial", 13)).grid(row=2, column=1, sticky="w")
+ub_lable = tk.Label(gui, text="Upper Bound: ", font=("arial semibold", 14)).grid(row=1, column=0, sticky="e")
+ub = tk.Entry(gui, textvariable = upb, font=("arial", 13)).grid(row=1, column=1, sticky="w")
 
-lb_lable = tk.Label(gui, text ="Lower Bound: ", font=("arial semibold", 14)).grid(row=3, column=0, sticky="e")
-lb = tk.Entry(gui, textvariable = lowb, font=("arial", 13)).grid(row=3, column=1, sticky="w")
+lb_lable = tk.Label(gui, text ="Lower Bound: ", font=("arial semibold", 14)).grid(row=2, column=0, sticky="e")
+lb = tk.Entry(gui, textvariable = lowb, font=("arial", 13)).grid(row=2, column=1, sticky="w")
 
-dec_lable = tk.Label(gui, text ="Amount of Decimals: ", font=("arial semibold", 14)).grid(row=4, column=0, sticky="e")
-dec = tk.Entry(gui, textvariable = deci, font=("arial", 13)).grid(row=4, column=1, sticky="w")
+dec_lable = tk.Label(gui, text ="Amount of Decimals: ", font=("arial semibold", 14)).grid(row=3, column=0, sticky="e")
+dec = tk.Entry(gui, textvariable = deci, font=("arial", 13)).grid(row=3, column=1, sticky="w")
 
-amt_lable = tk.Label(gui, text ="Amount of Numbers: ", font=("arial semibold", 14)).grid(row=5, column=0, sticky="e")
-amt = tk.Entry(gui, textvariable = amot, font=("arial", 13)).grid(row=5, column=1, sticky="w")
+amt_lable = tk.Label(gui, text ="Amount of Numbers: ", font=("arial semibold", 14)).grid(row=4, column=0, sticky="e")
+amt = tk.Entry(gui, textvariable = amot, font=("arial", 13)).grid(row=4, column=1, sticky="w")
 
-prttf_lable = tk.Label(gui, text ="Print to File: ", font=("arial semibold", 14)).grid(row=6, column=0, sticky="e")
-prttf = tk.Checkbutton(gui, variable=ptf, text=" ").grid(row=6, column=1, sticky="w")
+prttf_lable = tk.Label(gui, text ="Print to File: ", font=("arial semibold", 14)).grid(row=5, column=0, sticky="e")
+prttf = tk.Checkbutton(gui, variable=ptf, text=" ").grid(row=5, column=1, sticky="w")
 
-flnm_label = tk.Label(gui, text = "File Name: ", font=("arial semibold", 14)).grid(row=7, column=0, sticky="e")
-flnm = tk.Entry(gui, textvariable = filenm, font=("arial", 13)).grid(row=7, column=1, sticky="w")
+flnm_label = tk.Label(gui, text = "File Name: ", font=("arial semibold", 14)).grid(row=6, column=0, sticky="e")
+flnm = tk.Entry(gui, textvariable = filenm, font=("arial", 13)).grid(row=6, column=1, sticky="w")
 
-gen_button = tk.Button(gui, text = "Generate", command=lambda:rng(),  font=("arial bold", 18)).grid(row=8, column=0, columnspan=2, sticky="n")
+gen_button = tk.Button(gui, text = "Generate", command=lambda:rng(),  font=("arial bold", 18)).grid(row=7, column=0, columnspan=2, sticky="n")
 
 gui.mainloop()
